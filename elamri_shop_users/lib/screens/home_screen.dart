ba0bdd9230a9dfaa988d4be70bba9f6a1d62030c
dell <dell@DESKTOP-4P8U8AR>
2024-11-18 22:38:02 +0100

    Build the user app UI: The Implement of the Light & Dark Theme with provider state management

---
+++ elamri_shop_users/lib/screens/home_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
+import 'package:elamri_shop_users/providers/theme_provider.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Hello world",
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            ElevatedButton(onPressed: () {}, child: const Text("Hello world")),
+            SwitchListTile(
+              title: Text(
+                  themeProvider.getIsDarkTheme ? "Dark Mode" : "Light Mode"),
+              value: themeProvider.getIsDarkTheme,
+              onChanged: (value) {
+                themeProvider.setDarkTheme(themeValue: value);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
