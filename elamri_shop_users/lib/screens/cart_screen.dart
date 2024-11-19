5f822e1f13887eafdee3048edf8c6f93f1413cae
dell <dell@DESKTOP-4P8U8AR>
2024-11-19 16:23:15 +0100

    Build the user App UI: Create the empty cart screen

--- elamri_shop_users/lib/screens/cart_screen.dart
+++ elamri_shop_users/lib/screens/cart_screen.dart
@@ -1,6 +1,9 @@
+import 'package:elamri_shop_users/services/assets_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:elamri_shop_users/widgets/subtitle_screen.dart';
+import 'package:elamri_shop_users/widgets/empty_bag.dart';
 
 import '../widgets/title_text.dart';
 
@@ -9,9 +12,15 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: TitlesTextWidget(label: "CartScreen"),
+        child: EmptyBagWidget(
+          imagePath: AssetsManager.shoppingBasket,
+          title: "Your cart is empty",
+          subtitle:
+              "Looks like your cart is empty add something and make me happy",
+          buttonText: "Shop now",
+        ),
       ),
     );
   }
