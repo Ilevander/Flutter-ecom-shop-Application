df7b02af2b639871c9c1d13c67827873578af50a
dell <dell@DESKTOP-4P8U8AR>
2024-11-25 13:24:04 +0100

    Build user APP UI: Implement Logi Screen Design

--- elamri_shop_users/lib/main.dart
+++ elamri_shop_users/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:elamri_shop_users/root_screen.dart';
+import 'package:elamri_shop_users/screens/auth/login.dart';
 import 'package:elamri_shop_users/screens/inner_screen/product_details.dart';
 import 'package:elamri_shop_users/screens/inner_screen/viewed_recently.dart';
 import 'package:elamri_shop_users/screens/inner_screen/wishlist.dart';
@@ -29,7 +30,8 @@ class MyApp extends StatelessWidget {
           title: 'Elamri-Shop MA',
           theme: Styles.themeData(
               isDarkTheme: themeProvider.getIsDarkTheme, context: context),
-          home: const RootScreen(),
+          //home: const RootScreen(),
+          home: const LoginScreen(),
           routes: {
             ProductDetailsScreen.routName: (context) =>
                 const ProductDetailsScreen(),
