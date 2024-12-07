8d4155174c2aa466caa5633acb6b2599526c0d71
dell <dell@DESKTOP-4P8U8AR>
2024-12-07 20:36:36 +0100

    Admin Panel APP: Implement the Dashboard Screen

--- elamri_shop_admin/lib/screens/dashboard_screen.dart
+++ elamri_shop_admin/lib/screens/dashboard_screen.dart
@@ -1,3 +1,5 @@
+import 'package:elamri_shop_admin/models/dashboard_btn_model.dart';
+import 'package:elamri_shop_admin/widgets/dashboard_btn.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/theme_provider.dart';
@@ -17,27 +19,36 @@ class DashboardScreenState extends State<DashboardScreen> {
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const TitlesTextWidget(label: "Dashboard Screen"),
-        leading: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Image.asset(AssetsManager.shoppingCart),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              themeProvider.setDarkTheme(
-                  themeValue: !themeProvider.getIsDarkTheme);
-            },
-            icon: Icon(themeProvider.getIsDarkTheme
-                ? Icons.light_mode
-                : Icons.dark_mode),
+        appBar: AppBar(
+          title: const TitlesTextWidget(label: "Dashboard Screen"),
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(AssetsManager.shoppingCart),
           ),
-        ],
-      ),
-      body: const Center(
-        child: TitlesTextWidget(label: "Dashboard Screen "),
-      ),
-    );
+          actions: [
+            IconButton(
+              onPressed: () {
+                themeProvider.setDarkTheme(
+                    themeValue: !themeProvider.getIsDarkTheme);
+              },
+              icon: Icon(themeProvider.getIsDarkTheme
+                  ? Icons.light_mode
+                  : Icons.dark_mode),
+            ),
+          ],
+        ),
+        body: GridView.count(
+          crossAxisCount: 2,
+          children: List.generate(
+            DashboardButtonsModel.dashboardBtnList(context).length,
+            (index) => DashboardButtonsWidget(
+              text: DashboardButtonsModel.dashboardBtnList(context)[index].text,
+              imagePath: DashboardButtonsModel.dashboardBtnList(context)[index]
+                  .imagePath,
+              onPressed: DashboardButtonsModel.dashboardBtnList(context)[index]
+                  .onPressed,
+            ),
+          ),
+        ));
   }
 }
